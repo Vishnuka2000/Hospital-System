eccc1f190b42274758bf2d40b3ad09033d61d34e
Vishnuka2000 <vishnuka2019@gmail.com>
2025-09-12 15:54:45 +0530

    Patient Commit

---
+++ src/main/java/com/example/Hospital/Entity/Patient.java
@@ -0,0 +1,24 @@
+package com.example.Hospital.Entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class Patient {
+     @Id
+     @GeneratedValue (strategy =  GenerationType.IDENTITY)
+     private Long id;
+     private String name;
+     private String gender;
+     private  int age;
+}
